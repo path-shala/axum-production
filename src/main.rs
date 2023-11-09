373b35bab2a2bf0e283f9d086a257f75c3da0fc4
Avyukth <makur.subhrajit@gmail.com>
2023-11-10 03:37:17 +0530

    added static method

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use axum::Router;
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
 }
 
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello_2))
+}
+
+fn route_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./static")))
+}
+
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
-
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(route_static());
     let address = SocketAddr::from(([127, 0, 0, 1], 9090));
     println!("\n--->> Starting server on http://{address}\n");
     axum::Server::bind(&address)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
@@ -26,6 +38,14 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("--->> {:<12} handler_hello - {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World");
+    Html(format!(
+        "<p>Welcome to awesome <strong>{name}</strong> rust<p>"
+    ))
+}
+
+async fn handler_hello_2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("--->> {:<12} handler_hello - {name:?}", "HANDLER");
+
     Html(format!(
         "<p>Welcome to awesome <strong>{name}</strong> rust<p>"
     ))
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -3,6 +3,6 @@ use anyhow::{Ok, Result};
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://127.0.0.1:9090")?;
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello2/subhrajit").await?.print().await?;
     Ok(())
 }
